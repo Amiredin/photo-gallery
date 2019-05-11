81cfb2fbd5364dc1386dcea4a77c9bd03de058de
Amir Edin <amiredin@yahoo.com>
2019-05-11 10:10:36 +0300

    add views for welcome

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def welcome(request):
+    image = image.get_images()
+    context= {'images':images}
+    return render (request, 'index.html',context)
